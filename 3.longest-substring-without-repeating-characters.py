c3201de03bcdb082156c40981699ace99bb7a528
DuyNguyen555 <drduyvn555@gmail.com>
2022-06-21 16:46:54 +0700

    Create 94,100

---
+++ 3.longest-substring-without-repeating-characters.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode id=3 lang=python
+#
+# [3] Longest Substring Without Repeating Characters
+#
+
+# @lc code=start
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        charSet = set()
+        left = 0
+        res = 0
+
+        for right in range(len(s)):
+            while s[right] in charSet:
+                charSet.remove(s[left])
+                left += 1
+            
+            charSet.add(s[right])
+            res = max(res, right - left + 1)
+        
+        return res
+# @lc code=end
--- 94.binary-tree-inorder-traversal.py
+++ 94.binary-tree-inorder-traversal.py
@@ -18,6 +18,14 @@ class Solution(object):
         :type root: TreeNode
         :rtype: List[int]
         """
+        result = []
+        def inorder(root):
+            if not root:
+                return
+            inorder(root.left)
+            result.append(root.val)
+            inorder(root.right)
         
+        inorder(root)
+        return result
 # @lc code=end
-
